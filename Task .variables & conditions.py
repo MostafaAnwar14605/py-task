b5fe60c1009cd9e2ec8f21b1a45988a5cbba413b
MostafaAnwar1 <sasaanwar521@gmail.com>
2025-07-06 00:20:40 +0300

    solved the task

---
+++ Task .variables & conditions.py
@@ -0,0 +1,26 @@
+'''
+x = 10
+if x > 15:
+    print(x>15)
+else:
+    print('x is smaller than 15')
+'''
+
+'''
+name = 'mostafa'
+if name == 'mostafa':
+    print('Welcome Mostafa')
+
+'''
+
+'''
+numbers = [2, 4, 6, 8, 10]
+
+print(4 in numbers)
+
+print(4 in numbers and 6 in numbers)
+
+print(3 in numbers or 6 in numbers)
+
+print(2 in numbers and 4 in numbers and 5 in numbers)
+'''
---
+++ Task .variables & conditions.py2.py
@@ -0,0 +1,99 @@
+Python 3.11.9 (tags/v3.11.9:de54cf5, Apr  2 2024, 10:00:00) [MSC v.1938 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+x = false
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    x = false
+NameError: name 'false' is not defined. Did you mean: 'False'?
+x
+x = False
+x
+False
+type x
+SyntaxError: invalid syntax
+
+type(x)
+<class 'bool'>
+y = 10
+y
+10
+type(y)
+<class 'int'>
+z = 10.5
+z
+10.5
+type(z)
+<class 'float'>
+s = 'Anwar'
+s
+'Anwar'
+type(s)
+<class 'str'>
+f =float(y)
+f
+10.0
+type(f)
+<class 'float'>
+e = [1,2,3,4,5]
+e
+[1, 2, 3, 4, 5]
+type(e)
+<class 'list'>
+c = (10,11,12,13,14,15)
+c
+(10, 11, 12, 13, 14, 15)
+type(c)
+<class 'tuple'>
+g = tuple(e)
+g
+(1, 2, 3, 4, 5)
+type(g)
+<class 'tuple'>
+h = {1: mostafa , 2: ahmed , 3:anwar}
+Traceback (most recent call last):
+  File "<pyshell#27>", line 1, in <module>
+    h = {1: mostafa , 2: ahmed , 3:anwar}
+NameError: name 'mostafa' is not defined
+>>> h = {1: 'mostafa' , 2: 'ahmed' , 3:'anwar'}
+>>> h
+{1: 'mostafa', 2: 'ahmed', 3: 'anwar'}
+>>> type(h)
+<class 'dict'>
+>>> x =10
+>>> x +=15
+>>> x
+25
+>>> x /= 5
+>>> x
+5.0
+>>> x *=10
+>>> x
+50.0
+>>> x %= 3
+>>> x
+2.0
+>>> 
+= RESTART: C:/Users/pc123/OneDrive/Desktop/Data Science course/The Tasks/Task .variables & conditions.py
+3
+>>> 11%4
+3
+>>> 2**3
+8
+>>> 11/3
+3.6666666666666665
+>>> 11//3
+3
+>>> 10>15
+False
+>>> 
+= RESTART: C:/Users/pc123/OneDrive/Desktop/Data Science course/The Tasks/Task .variables & conditions.py
+x is smaller than 15
+>>> 
+= RESTART: C:/Users/pc123/OneDrive/Desktop/Data Science course/The Tasks/Task .variables & conditions.py
+Welcome Mostafa
+>>> 
+= RESTART: C:/Users/pc123/OneDrive/Desktop/Data Science course/The Tasks/Task .variables & conditions.py
+True
+True
+True
+False
